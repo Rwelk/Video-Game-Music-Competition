e1a40eb4e19da3f3378e95bb3c94f66650821e62
Rwelk <56904170+Rwelk@users.noreply.github.com>
2022-03-06 23:47:39 -0700

    Creates the Review Slide

--- vgmc_pp_maker.py
+++ vgmc_pp_maker.py
@@ -3,7 +3,7 @@
 from pptx import Presentation
 from pptx.util import Inches, Pt
 from pptx.enum.text import MSO_VERTICAL_ANCHOR
-from math import ceil
+from math import ceil, floor
 
 
 import copy
@@ -31,7 +31,7 @@ def main():
 	print(f"\033[96mThere will be \033[93m{num_rounds}\033[96m Rounds Each with \033[93m{num_tracks}\033[96m Tracks.\033[0m")
 
 	# Create the PowerPoint using the slide template.
-	prs = Presentation(ROOT / "template.pptx")
+	prs = Presentation(ROOT / "templates" / "master_copy.pptx")
 
 	# Create the Title and Rules Slides
 	rules_slide(prs, num_rounds, num_tracks)
@@ -51,18 +51,21 @@ def main():
 	save_presentation(prs)
 
 
-# This method reads the arguements passed in to determine how many Rounds and Tracks there will be.
+# This method reads the arguements passed in to determine how many Rounds and
+# 	Tracks there will be.
 def determine_parameters():
 
-	# default is supposed to be mutually exclusive with rounds and tracks, so if -d is provided, it cannot
-	#	 be combined with -r or -t.
-	# One the other end, -r and -t can be used together or separatly, but cannot be used with -d.
+	# default is supposed to be mutually exclusive with rounds and tracks, so if
+	#     -d is provided, it cannot be combined with -r or -t.
+	# One the other end, -r and -t can be used together or separatly, but cannot
+	#     be used with -d.
 	if args.default and (args.rounds or args.tracks):
 		print("--default and --rounds|--tracks are mutually exclusive and cannot be used together.")
 		sys.exit(2)
 	else:
 
-		# If -d was provided, set rounds and tracks to the default values of 5 and 10 respectively.
+		# If -d was provided, set rounds and tracks to the default values of 5
+		#     and 10 respectively.
 		if args.default: rounds, tracks = 5, 10
 
 		# Otherwise, check to see if -r and/or -t was/were provided.
@@ -90,19 +93,23 @@ def add_slide(prs, layout, title):
 # This method is for adding additional lines of text to a slide
 def add_text(slide, text, level=0, bold=False, italic=False):
 
-	# Each slide has some number of items on them called shapes, which themselves store some number of
-	#	 placeholders.
-	# These placeholders are stored in a dictionary simply called placeholders{}.
+	# Each slide has some number of items on them called shapes, which
+	# 	themselves store some number of placeholders.
+	# These placeholders are stored in a dictionary within the slide's shapes
+	# 	called placeholders{}.
 	# To add text to a slide, we have to access the placeholder stored at key 1.
-	# Note that since placeholders is not an array, this is not accessing INDEX 1 but KEY 1.
-	# From there, we can acess the text_frame, which is what stores the text in the shape.
+	# Note that since placeholders is not an array, this is actually accessing 
+	# 	KEY 1, NOT INDEX 1.
+	# From there, we can access the text_frame, which is what stores the text in
+	# 	the shape.
 	content_area = slide.shapes.placeholders[1].text_frame
 
 	# text_frames are composed of various Paragraphs.
 	# p will be the Paragraph that our text argument will be written into.
-	# However, before we can write we have to determine whether the text_frame already has content.
-	# If it does, we have to call .add_paragraph(), a method belonging to text_frame that creates a new
-	#	 Paragraph that can be written to.
+	# However, before we can write we have to determine whether the text_frame
+	# 	already has content.
+	# If it does, we have to call .add_paragraph(), a method belonging to
+	# 	text_frame that creates a new Paragraph that can be written to.
 	# Otherwise, we should write to the Paragraph already there.
 	p = content_area.add_paragraph() if content_area.text else content_area.paragraphs[0]
 
@@ -115,48 +122,9 @@ def add_text(slide, text, level=0, bold=False, italic=False):
 	p.text = text
 
 
-# This method creates the Rules Slide.
-def rules_slide(prs, rounds, tracks):
-
-	# Create the Rules Slide.
-	slide = add_slide(prs, prs.slide_layouts[1], "Rules and Scoring")
-
-	# Add text to the Rules Slide.
-	add_text(slide, f"There will be {rounds} Rounds of {tracks} Tracks")
-	add_text(slide, "You will get roughly 30 – 45 seconds of music to guess from.")
-	add_text(slide, "Scoring is as follows:")
-	add_text(slide, "1 point for Game Franchise", level=1)
-	add_text(slide, "1 point for Specific Game", level=1)
-	add_text(slide, "1 point for Track Name/Place", level=1)
-	add_text(slide, "A couple songs don’t have official releases, or play in multiple places. Those have a star listed on the answer key, and if you put something close to it you’ll still receive the point.")
-
-
-# This method is for making the slides that show the round number before a round.
-def round_slide(prs, round_num):
-
-	# Create the slide that announces the start of a new Round.
-	slide = add_slide(prs, prs.slide_layouts[2], f"Round {round_num}")
-
-
-# This method is for making the individual track slides for each round.
-def track_slide(prs, round_num, track_num):
-
-	# Create and title the slide.
-	slide = add_slide(prs, prs.slide_layouts[3], f"Track {track_num}")
-
-	# Add the Audio object.
-	slide.shapes.add_movie(
-		FileIO(ROOT / f"tracks/{round_num}-{track_num}.mp3", "rb"),
-		left=Inches(8), top=Inches(1.5), width=Inches(6), height=Inches(6),
-		poster_frame_image=FileIO(ROOT / "speaker.png", "rb"),
-		mime_type='audio/mp3',
-	)
-
-
-
-
+# This method is for duplicating a shape and adding it to the slide on which it
+# 	appears.
 def clone_shape(shape):
-	"""Add a duplicate of `shape` to the slide on which it appears."""
 	shape_obj = shape.element
 	sp_tree = shape_obj.getparent()
 	new_sp = copy.deepcopy(shape_obj)
@@ -169,77 +137,156 @@ def clone_shape(shape):
 	return new_shape
 
 
+# This method creates the Rules slide.
+def rules_slide(prs, rounds, tracks):
+
+	# Create and title the slide.
+	slide = add_slide(prs, prs.slide_layouts[1], "Rules and Scoring")
+
+	# Add text to the Rules Slide.
+	add_text(slide, f"There will be {rounds} Rounds of {tracks} Tracks")
+	add_text(slide, "You will get roughly 30 – 45 seconds of music to guess from.")
+	add_text(slide, "Scoring is as follows:")
+	add_text(slide, "1 point for Game Franchise", level=1)
+	add_text(slide, "1 point for Specific Game", level=1)
+	add_text(slide, "1 point for Track Name/Place", level=1)
+	add_text(slide, "A couple songs don’t have official releases, or play in multiple places. Those have a star listed on the answer key, and if you put something close to it you’ll still receive the point.")
 
 
+# This method creates the slides that show the round number before each round.
+def round_slide(prs, round_num):
+
+	# Create and title the slide.
+	slide = add_slide(prs, prs.slide_layouts[2], f"Round {round_num}")
 
 
-# This method is for making the Review slide at the end of each round.
+# This method creates the individual Track slides for each round.
+def track_slide(prs, round_num, track_num):
+
+	# Create and title the slide.
+	slide = add_slide(prs, prs.slide_layouts[3], f"Track {track_num}")
+
+	# Add the Audio object.
+	slide.shapes.add_movie(
+		FileIO(ROOT / f"tracks/{round_num}-{track_num}.mp3", "rb"),
+		left=Inches(8), top=Inches(1.5), width=Inches(6), height=Inches(6),
+		poster_frame_image=FileIO(ROOT / "templates" / "speaker.png", "rb"),
+		mime_type='audio/mp3',
+	)
+
+
+# This method creates the Review slide for relistening to every track in the
+# 	round.
 def review_slide(prs, round_num, num_tracks):
 
+	# Create and title the slide.
 	slide = add_slide(prs, prs.slide_layouts[4], f"Round {round_num} Review")
 
-
-	audio_base_x = 5.4
-	audio_base_y = 1.11
-
-	paragraph_base_x = 7.56
-	paragraph_base_y = 4
-
+	# Create a reference to the base text placeholder that will be copied.
 	template_text_box = slide.shapes.placeholders[10]
 
 
+	# Generate the song-text pairs.
+	for i in range(1, num_tracks + 1):
 
+		# Add the track.
+		track = slide.shapes.add_movie(
 
-	
-	# # Set the parameters of the text box already there.
-	# first_item = slide.shapes.placeholders[1]
-	# first_item.left = Inches(paragraph_base_x)
-	# first_item.top = Inches(paragraph_base_y)
+			# Location of the track.
+			FileIO(ROOT / "tracks" / f"{round_num}-{i}.mp3", "rb"),
 
-	# fi_tb = first_item.text_frame.paragraphs[0]
-	# fi_tb.text = " Track 1"
+			# X-coordinate of the track
+			left=Inches(determine_x_coord(i, "track")),
+			
+			# Y-coordinate of the track
+			top=Inches(determine_y_coord(num_tracks, i)),
+			
+			# Dimensions of the track
+			width=Inches(1), height=Inches(1),
 
-	# Set parameters for every other item.
-	for i in range(0, num_tracks):
-		copied_text_box = clone_shape(template_text_box)
-		copied_text_box.left = Inches(
-			paragraph_base_x 
-			if i % 2 == 0 else 
-			paragraph_base_x + 4.49
+			# Extra details
+			poster_frame_image=FileIO(ROOT / "templates" / "speaker.png", "rb"),
+			mime_type='audio/mp3'
 		)
 
-		copied_text_box.top = Inches(
-			4 - (
-				ceil(num_tracks / 4) - (i // 2) - (
-					1 if ceil(num_tracks / 2) % 2 != 0 else 0.5
-				)
-			)
-		)
 
-		copied_text_box.text_frame.paragraphs[0].text = f" Track {i + 1}"
+		# Add the text box.
+		# Start by cloning the template text placeholder.
+		cloned_tb = clone_shape(template_text_box)
 
+		# Set the X-coordinate.
+		cloned_tb.left = Inches(determine_x_coord(i, "text"))
 
-	# Finally, delete the extra template text box that was copied from.
+		# Set the Y-coordinate.
+		cloned_tb.top = Inches(determine_y_coord(num_tracks, i))
+
+		# Add the text.
+		cloned_tb.text = f" Track {i}"
+
+		# If num_tracks is odd, the final Track and Text Box are sitting alone
+		# 	in the left column with no corresponding item in the right column,
+		# 	so horizontally center it.
+		if (i == num_tracks) and (num_tracks % 2 != 0):
+			track.left = Inches(8.75)
+			cloned_tb.left = Inches(9.75)
+		
+
+	# Finally delete the template text box.
 	sp = template_text_box._sp
 	sp.getparent().remove(sp)
 
 
-	# for i in range(1, num_tracks):
+# Equation for determining where the Review slide's Track and Text Box
+# 	X-coordinates are.
+def determine_x_coord(i, type):
 
-	# 	slide.shapes.add_movie(
-	# 		FileIO(ROOT / f"tracks/{round_num}-{i + 1}.mp3", "rb"),
-	# 		left=Inches(audio_base_x), top=Inches((1.1 * i) + audio_base_y), width=Inches(0.9), height=Inches(0.9),
-	# 		poster_frame_image=FileIO(ROOT / "speaker.png", "rb"),
-	# 		mime_type='audio/mp3'
-	# 	)
+	# The starting value is determined by if the X-coordinate is for the slide's
+	# 	Track or Text Box
+	start = 6.5 if (type == "track") else 7.5
+	
+	# The offset is for if the item is supposed to go in the left column or the
+	# 	right column.
+	offset = 0 if (i % 2 != 0) else 4.5
 
-	# 	tb = slide.shapes.add_textbox(
-	# 		left=Inches(paragraph_base_x), top=Inches((1.09 * i) + paragraph_base_y), 
-	# 		width=Inches(2.68), height=Inches(0.71))
-		
-	# 	tb_text = tb.text_frame.paragraphs[0]
-	# 	tb_text.font.size = Pt(36)
-	# 	tb_text.text = f"- Track {i + 1}"
+	return start + offset
+
+
+# Equation for determining where the Review slide's Track and Text Box
+# 	Y-coordinates are.
+def determine_y_coord(n, i):
+
+	# Set how far apart the items will be.
+	SPREAD = 1.5
+
+	# 4 will place the item right in the middle of the slide.
+	# To shift the item up or down depending on i, the following equation is
+	# 	evaluated:
+	return 4 - (round(
+
+		# y = m * x + b, where
+		# m is -SPREAD
+		# x is ceil(i / 2) to account for the left and right columns
+		# b is (SPREAD * ceil(n / 2)) to find the base y-intercept
+		-SPREAD * ceil(i / 2) + (SPREAD * ceil(n / 2))
+
+		# Next make slight adjustments to the above equation.
+		# Subtract the amount required to place the middle items along the
+		# 	center of the slide.
+		- (SPREAD * floor(n / 4))
+
+		# If ceil(n / 2) % 2 == 0, then there will be an even number of rows, so
+		# 	everything has to be futher offset by a small amount.
+		- (
+			0
+			if ceil(n / 2) % 2 != 0 else
+
+			# The offset is either positive or negative depending on if n is a
+			# 	multiple of 4.
+			((SPREAD / 2) if n % 4 != 0 else -(SPREAD / 2))
+		)
+	, 3))
+
+
 
 
 # This method is for saving the PowerPoint produced by this script.
